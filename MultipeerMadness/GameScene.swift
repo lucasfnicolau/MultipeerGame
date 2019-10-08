d8323f84f91516e41d359374c3a9708be0c5390e
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-10-08 17:48:13 -0300

    Conexão de multiplos jogadores funcionando.

--- MultipeerMadness/GameScene.swift
+++ MultipeerMadness/GameScene.swift
@@ -11,75 +11,48 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var circles = [SKShapeNode]()
+    var colors: [UIColor] = [#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)]
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
+    func addPlayer(index: Int) {
+        let circle = SKShapeNode(circleOfRadius: 10)
+        circle.fillColor = colors[index]
+        circles.append(circle)
+        
+        self.addChild(circle)
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let first = touches.first else { return }
+        let index = GameViewController.peerID.pid
+        circles[index].position = first.location(in: self)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     
--- MultipeerMadness/GameViewController.swift
+++ MultipeerMadness/GameViewController.swift
@@ -9,21 +9,53 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import MultipeerConnectivity
 
-class GameViewController: UIViewController {
+class CustomMCPeerID: MCPeerID {
+    var pid: Int = -1
+    
+    override init(displayName myDisplayName: String) {
+        super.init(displayName: myDisplayName)
+    }
+    
+    init(pid: Int, withDisplayName displayName: String) {
+        super.init(displayName: displayName)
+        self.pid = pid
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class GameViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+    
+    static var peerID: CustomMCPeerID!
+    static var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
+    var messageToSend: String!
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showConnectionMenu))
+        
+        GameViewController.peerID = CustomMCPeerID(displayName: UIDevice.current.name)
+        GameViewController.mcSession = MCSession(peer: GameViewController.peerID, securityIdentity: nil, encryptionPreference: .required)
+        GameViewController.mcSession.delegate = self
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            scene = GameScene(fileNamed: "GameScene")
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
@@ -46,5 +78,104 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func showConnectionMenu() {
+        let ac = UIAlertController(title: "Connection Menu", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: hostSession))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func hostSession(action: UIAlertAction) {
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "ioscreator-chat", discoveryInfo: nil, session: GameViewController.mcSession)
+        mcAdvertiserAssistant.start()
+        GameViewController.peerID.pid = 0
+        scene.addPlayer(index: 0)
+    }
+
+    func joinSession(action: UIAlertAction) {
+        let mcBrowser = MCBrowserViewController(serviceType: "ioscreator-chat", session: GameViewController.mcSession)
+      mcBrowser.delegate = self
+      present(mcBrowser, animated: true)
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+      switch state {
+      case .connected:
+        
+        let playersNumber = GameViewController.mcSession.connectedPeers.count
+        if GameViewController.peerID.pid != 0 {
+            GameViewController.peerID.pid = playersNumber
+        }
+        
+        if (GameViewController.peerID.pid == 0) {
+            send("players:\(playersNumber)")
+            scene.addPlayer(index: playersNumber)
+        }
+        
+        print("Connected: \(GameViewController.peerID.displayName)")
+      case .connecting:
+        print("Connecting: \(GameViewController.peerID.displayName)")
+      case .notConnected:
+        print("Not Connected: \(GameViewController.peerID.displayName)")
+      @unknown default:
+        print("fatal error")
+      }
+    }
+    
+    func send(_ value: String) {
+        
+        let data = value.data(using: .utf8)
+        do {
+            try GameViewController.mcSession.send(data!, toPeers: GameViewController.mcSession.connectedPeers, with: .unreliable)
+        }
+        catch {
+            print("Error sending message")
+        }
+    }
+
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+      DispatchQueue.main.async { [unowned self] in
+    
+        guard let value = String(data: data, encoding: .utf8) else { return }
+        let keyValue = value.split(separator: ":")
+        
+        if keyValue[0] == Substring("players") {
+            let playersNumber = Int(keyValue[1]) ?? 1
+            for index in self.scene.circles.count ... playersNumber {
+                self.scene.addPlayer(index: index)
+            }
+        } else {
+
+        }
+        
+      }
+    }
+
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+
+    }
+
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+
+    }
+
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+
+    }
+
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        
+        dismiss(animated: true)
+    }
+
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
     }
 }
