321c11becfb3460fbafd13a1e990df0b35ef40be
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-10-22 13:50:12 -0300

    Colisões iniciadas e normalização de posições implementada (com ajustes a serem feitos)

---
+++ MultipeerMadness/Contact/GameSceneContactDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  GameSceneContactDelegate.swift
+//  MultipeerMadness
+//
+//  Created by Lucas Fernandez Nicolau on 21/10/19.
+//  Copyright © 2019 {lfn}. All rights reserved.
+//
+
+import Foundation
